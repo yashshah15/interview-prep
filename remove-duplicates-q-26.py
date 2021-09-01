3c04018c0208c84039878607a76060fffdbf4d71
Yash Shah <yashshah518@gmail.com>
2021-09-01 15:57:10 -0700

    added valid reomve duplicates from array

---
+++ remove-duplicates-q-26.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        j=1
+        for i in range(len(nums)-1):
+            if (nums[i] != nums[i+1]):
+                    nums[j]=nums[i+1]
+                    j=j+1
+        return j
