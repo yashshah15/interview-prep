25fdc460ddc178035ff5e9cb7641779c0d414907
yashshah15 <45618114+yashshah15@users.noreply.github.com>
2022-01-07 13:26:00 -0800

    Time: 56 ms (94.41%), Space: 15.6 MB (91.89%) - LeetHub

--- 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
+++ 116-populating-next-right-pointers-in-each-node/116-populating-next-right-pointers-in-each-node.py
@@ -14,31 +14,22 @@ class Solution:
         if not root:
             return root
         
-        # Start with the root node. There are no next pointers
-        # that need to be set up on the first level
-        leftmost = root
         
+        leftptr = root
         # Once we reach the final level, we are done
-        while leftmost.left:
+        while leftptr.left:
+
             
-            # Iterate the "linked list" starting from the head
-            # node and using the next pointers, establish the 
-            # corresponding links for the next level
-            head = leftmost
+            head = leftptr
+            
             while head:
-                
-                # CONNECTION 1
                 head.left.next = head.right
                 
-                # CONNECTION 2
                 if head.next:
                     head.right.next = head.next.left
                 
-                # Progress along the list (nodes on the current level)
                 head = head.next
             
-            # Move onto the next level
-            leftmost = leftmost.left
-        
-        return root 
+            leftptr = leftptr.left
+        return root
                 
