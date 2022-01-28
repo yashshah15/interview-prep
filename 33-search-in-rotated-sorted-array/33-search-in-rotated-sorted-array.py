d291b6de3907658dcc5a54183099ce1f8a2e7436
yashshah15 <45618114+yashshah15@users.noreply.github.com>
2022-01-27 18:43:22 -0800

    Time: 60 ms (33.41%), Space: 14.4 MB (99.00%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -0,0 +1,31 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        beg = 0
+        end = len(nums) - 1
+        mid = (beg + end) // 2
+
+        
+        while nums[mid] != target and beg <= end:
+            #array from beg to mid is not rotated
+            if nums[mid] >= nums[beg]:
+                #check if the array lies in this half
+                if target >= nums[beg] and target <= nums[mid]:
+                    end  = mid - 1
+                #target may exist in the other half
+                else:
+                    beg = mid + 1
+            #Situation where the array from beg to mid is rotated
+            elif nums[mid] <= nums[beg]:
+                if target >= nums[mid] and target <= nums[end]:
+                    beg = mid + 1
+                
+                else:
+                    end = mid - 1
+            
+            mid = (beg + end) // 2
+        
+        if nums[mid] == target:
+            return mid
+        
+        else:
+            return - 1
